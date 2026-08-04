6102b9b8de8c3c87fcba7ca0afc8f526f3ae84d4
yaraSchneider <yara_schneider@estudante.sesisenai.org.br>
2026-08-03 21:29:12 -0300

    Add Windows support and initial project setup for Flutter application
    - Created CMakeLists.txt for Windows build configuration.
    - Added manifest.json for web application configuration.
    - Implemented Windows runner files including main application logic, window management, and resource handling.
    - Included necessary headers and utility functions for command line argument parsing and console management.
    - Established plugin registration mechanism with generated_plugin_registrant files.
    - Added resource files for application icon and versioning.
    - Configured .gitignore for Windows-specific files and directories.

---
+++ Hello_InovaMobile/lib/test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MeuWidget extends StatelessWidget {
+  const MeuWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
